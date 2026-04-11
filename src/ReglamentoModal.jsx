ca24987ad46585821c437e396b9a53a2509690cb
Ariel Barotto <abarotto83@gmail.com>
2026-04-11 18:59:24 -0300

    modal

--- src/ReglamentoModal.jsx
+++ src/ReglamentoModal.jsx
@@ -7,7 +7,7 @@ function ReglamentoModal({ onClose }) {
       <div className="modal-content">
         <h2>Reglamento Prode Mundial FIFA 2026</h2>
         <ol>
-          <li>Pueden participar únicamente empleados de <strong>Molinos Florencia S.A.U.</strong>. La participación es gratuita y voluntaria.</li>
+          <li>Pueden participar únicamente empleados de <strong>Molinos Florencia S.A.U.</strong>. La participación es <strong>gratuita y voluntaria</strong>.</li>
           <li>El juego abarca sólo la primera fase del Mundial FIFA 2026 (72 partidos desde el <strong>11/06/26</strong>).</li>
           <li>Para participar se debe ingresar a{" "} 
             <a href="https://prodemolinosflorencia.netlify.app" target="_blank" rel="noopener noreferrer">
@@ -22,6 +22,9 @@ function ReglamentoModal({ onClose }) {
               <li>Sin acierto: <strong>0 pts</strong></li>
             </ul>
           </li>
+          <li>Una vez confirmados los pronósticos no se pueden modificar.</li>
+          <li>Límite para confirmar: <strong>10/06/26 a las 12:00 hs</strong>. El usuario que haya guardado los pronósticos y no los haya confirmado no será tenido en cuenta.</li>
+          <li>Una vez confirmado los pronósticos cada participante puede acceder a ver sus pronósticos, los resultados oficiales y el ranking de participantes.</li>
           <li>Ganadores: 
             <ul>
               <li>Mayor cantidad de puntos.</li>
@@ -30,10 +33,6 @@ function ReglamentoModal({ onClose }) {
               <li>En caso de terminar 2 ó más participantes con la misma cantidad de puntos y de aciertos exactos en el primer puesto el orden de cada participante se definirá por sorteo.</li>
             </ul>
           </li>
-          <li>Una vez confirmados los pronósticos no se pueden modificar.</li>
-          <li>Límite para confirmar: <strong>10/06/26 a las 12:00 hs</strong>. El usuario que haya guardado los pronósticos y no los haya confirmado no será tenido en cuenta.</li>
-          <li>Una vez confirmado los pronósticos cada participante puede acceder a ver sus pronósticos, los resultados oficiales y el ranking de participantes.</li>
-          <li>El juego no tiene costo.</li>
           <li><strong>Premios:</strong>
             <ul>
               <li>1er Premio: Una cena para dos personas</li>
